8760c5b465be19eb02a747a6d8ec395a9fcc90fa
INZAF EBRAHIM <inzafebrahim@jamesBond.local>
2023-11-15 12:44:54 +0530

    new simple  pattern

---
+++ Patterns/simpe-star.js
@@ -0,0 +1,9 @@
+
+let store = "";
+for (let i = 0; i < 5; i++) {
+    for(let j =0 ;j<=i;j++){
+        store +='*'
+    }
+    store +='\n'
+}
+console.log(store);
--- Patterns/square-star.js
+++ Patterns/square-star.js
@@ -1,8 +1,63 @@
-let a = "";
+// let a = "";
+// for (let i = 0; i < 5; i++) {
+//     for(let j= 0; j< 5; j++){
+//             a += "*"
+//     }
+//     a += '\n'
+// }
+// console.log(a);
+
+// let store = "";
+// for (let i = 0; i < 4; i++) {
+//     if(i==0 ||i==3){
+//             store += "*"
+//     }else {store +=" " }
+// }
+//     store += '\n'
+//     for(let j=0;j<4 ;j++){
+//     if(j==0 || j==3){
+//         store += " "
+//     }else {store +="*" }
+//     }
+//     store += '\n'
+//     for (let k = 0; k < 4; k++) {
+//     if(k==0 ||k==3){
+//             store += "*"
+//     }else {store +=" " }
+// }
+
+// console.log(store);
+
+
+// p1
+
+let store = "";
 for (let i = 0; i < 5; i++) {
-    for(let j= 0; j< 5; j++){
-            a += "*"
+    for(let j =0 ;j<=i;j++){
+        store +='*'
     }
-    a += '\n'
+    store +='\n'
 }
-console.log(a);
+console.log(store);
+
+// p2
+
+// let store ='';
+// for(let i= 5;i>0;i--){
+//     for(let j=0 ;j<i;j++){
+//         store +='*'
+//     }
+//     store +='\n'
+// }
+// console.log(store);
+
+// p3
+
+// let store ='';
+// for(let i= 5;i>0;i--){
+//     for(let j=0 ;j<i;j++){
+//         store +=' .'
+//     }
+//     store +='\n'
+// }
+// console.log(store);
